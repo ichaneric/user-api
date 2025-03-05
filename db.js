f584db8d20abb0adc99498eb84ac375197079920
Christian Eric Sanchez <152345045+ichaneric@users.noreply.github.com>
2025-03-05 08:48:08 +0800

    Init commit

---
+++ db.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('users.db');
+
+// Create the users table if it doesn't exist
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  )
+`);
+
+module.exports = db;
